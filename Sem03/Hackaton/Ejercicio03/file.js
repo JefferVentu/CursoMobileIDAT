822afddb90c2706a1204ca98e9bd75a7c954ae5f
Jeffer <jvruiz62@gmail.com>
2024-07-24 13:28:42 -0500

    Hackacton 03

--- Sem03/Hackaton/Ejercicio03/file.js
+++ Sem03/Hackaton/Ejercicio03/file.js
@@ -1,6 +1,27 @@
 let numerosPrimos = [];
-let i=0;
 
-do{
-    let primos = i;
-}while(numerosPrimos.length < 11)
+function primos(num){
+    for(let i=2; i < num; i++){
+        if(num%i===0){
+            return false;
+        }
+    }
+    return num !== 1;
+}
+
+for (let i=1; i<50; i++){
+    if(primos(i)){
+        numerosPrimos.push(i);
+        if(numerosPrimos.length==10){
+            break;
+        }
+    }
+}
+
+console.log(numerosPrimos);
+
+let pares = numerosPrimos.filter((valor)=> valor%2==0);
+let impares = numerosPrimos.filter((valor)=> valor%2!=0);
+
+console.log(pares);
+console.log(impares);
