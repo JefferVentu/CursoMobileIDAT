fc67d6edf54068653811a62ae9642c21f228e8ba
JefferVentu <jvruiz62@gmail.com>
2024-07-26 11:03:58 -0500

    no se xd

---
+++ Sem04/project2-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>
+        Hola estamos iniciando React
+      </p>
+    </div>
+  );
+}
+
+export default App;
