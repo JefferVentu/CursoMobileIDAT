5cefb909a49bbd66811832e8f5240a83e700e020
JefferVentu <jvruiz62@gmail.com>
2024-07-27 21:52:17 -0500

    Update Figure.jsx

--- Sem04/S02/singlePage-vite/src/components/Figure.jsx
+++ Sem04/S02/singlePage-vite/src/components/Figure.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Figure = () => {
   return (
     <div className='pr-44 flex items-end relative'>
-      <img src="{MyPicture}" className='z-10 alt="here is my picture"'/>
-      <div className="bg-orange-400 w-96 h-96 lg:w-[1000px] lg:h-[1000px] rounded-full absolute -right-60 -bottom-96"></div>
+      <img src={MyPicture} className='z-10' alt="here is my picture"/>
+      <div className="bg-orange-400 w-96 h-96 lg:w-[1000px] lg:h-[1000px] rounded-full absolute -right-45 -bottom-100 -z-10"></div>
 
 
     </div>
