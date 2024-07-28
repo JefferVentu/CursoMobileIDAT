7d1326595bb34f749e56410ad926a4a21577295b
JefferVentu <jvruiz62@gmail.com>
2024-07-28 14:54:25 -0500

    asd

--- Sem05/S01Teoria01/src/App.jsx
+++ Sem05/S01Teoria01/src/App.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 // Permite usar variables de estado dentro de un componente funcional. Nos restorna siempre un array con 2 elementos
 
 // Ejemplo 01
-
+/*
 const App = () => {
 
   const [count, setCount]= useState(0);
@@ -19,6 +19,25 @@ const App = () => {
       App
       <h1>Welcome to the counter of live</h1>
       <button onClick={handleClick}>Count is: {count}</button>
+    </div>
+  )
+}
+
+export default App
+*/
+
+// Ejemplo 02 - formulario
+const App = () => {
+  
+  const [user, setUser] = useState();
+  const [email, setEmail] = useState();
+  
+  return (
+    <div className='text-center'>
+      <form action="">
+        <input type="text" placeholder='Ingrese sus nombres'/>
+        <input type="text" placeholder='Ingrese su email'/>
+      </form>
     </div>
   )
 }
