ac87c7dfa016328d7e06ea53b36ca81df3f29bb8
JefferVentu <jvruiz62@gmail.com>
2024-08-19 10:56:06 -0500

    lo mismo

---
+++ Sem06/proyect03S02/proyect04S02/app/index.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+// COmponentes básicos
+// View:es el componente más básico para el UI. Es como un <div> y actua como un componente contendor que puede agrupar los componentes secundarios 
+// Text: es otro componente fundamental del react native. se puede utilizar en cualquier lugar dentro de la aplicación para mostrar datos textuales.
+// Image: procesa y muestra imágenes. Se puede utilizar para mostrar imagenes estáticas y noo estáticas. Además se puede utilizar para mostrar recursos que no son de imagen y archivos.
+// TextInput: Es el componente que maneja la entrada del usuario, es una parte crucial de la creación de una aplicación interactiva
+// ScrollView: 
+// StyleSheet
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
