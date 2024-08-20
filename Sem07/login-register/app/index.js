c71a729ae302ff643e6e87bbe02b4ecd4d431fb8
JefferVentu <jvruiz62@gmail.com>
2024-08-20 16:12:38 -0500

    actualización

--- Sem07/login-register/app/index.js
+++ Sem07/login-register/app/index.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import LoginScreen from '../screens/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen'
 
 export default function index() {
   return (
     <View>
-      <Text>index</Text>
+      <LoginScreen/>
+      <RegisterScreen/>
     </View>
   )
 }
