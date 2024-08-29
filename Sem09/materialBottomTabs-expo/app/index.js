c3f75c3fd51038117d9152b40399bef56c10f726
JefferVentu <jvruiz62@gmail.com>
2024-08-29 16:33:51 -0500

    sem09p4

--- Sem09/materialBottomTabs-expo/app/index.js
+++ Sem09/materialBottomTabs-expo/app/index.js
@@ -4,15 +4,38 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import HomeScreen from '../components/HomeScreen';
 import ProfileScreen from '../components/ProfileScreen';
 import SettingsScreen from '../components/SettingsScreen';
+import { AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function index() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={HomeScreen}/>
-            <Tab.Screen name='Profile' component={ProfileScreen}/>
-            <Tab.Screen name='Settings' component={SettingsScreen}/>
+        <Tab.Navigator
+            initialRouteName='Home'
+            activeColor='red'
+            barStyle={{backgroundColor: '#FF80ED'}}
+        >
+            <Tab.Screen 
+                name='Home' 
+                component={HomeScreen}
+                options={{
+                    tabBarIcon:()=><AntDesign name='home' size={25} style={{color: 'blue'}}/>
+                }}
+            />
+            <Tab.Screen 
+                name='Profile' 
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon:()=><MaterialCommunityIcons name='account' size={25} style={{color: 'blue'}}/>
+                }}
+            />
+            <Tab.Screen 
+                name='Settings' 
+                component={SettingsScreen}
+                options={{
+                    tabBarIcon:()=><Ionicons name='settings' size={25} style={{color: 'blue'}}/>
+                }}
+            />
         </Tab.Navigator>
     )
 }
