7b17710f3e708f46c72df929db301efa30acd48f
JefferVentu <jvruiz62@gmail.com>
2024-09-26 15:57:44 -0500

    pantalla home

--- Proyecto - RutasSM/RutasSM2/components/Home.js
+++ Proyecto - RutasSM/RutasSM2/components/Home.js
@@ -1,14 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import VerParaderos from './VerParaderos';
+import PointToPoint from './PointToPoint';
+import SearchRoutes from './SearchRoutes';
+
+const Tab = createBottomTabNavigator();
 
 const Home = () => {
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Ver Paraderos" component={VerParaderos} />
+            <Tab.Screen name="Punto a Punto" component={PointToPoint} />
+            <Tab.Screen name="Buscar Rutas" component={SearchRoutes} />
+        </Tab.Navigator>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+})
---
+++ Proyecto - RutasSM/RutasSM2/components/PointToPoint.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PointToPoint = () => {
+    return (
+        <View>
+            <Text>PointToPoint</Text>
+        </View>
+    )
+}
+
+export default PointToPoint
+
+const styles = StyleSheet.create({})
