e81826e83d62520c350dd52bda3bc00e500389df
JefferVentu <jvruiz62@gmail.com>
2024-09-26 16:30:20 -0500

    correción home

--- Proyecto - RutasSM/RutasSM2/components/Home.js
+++ Proyecto - RutasSM/RutasSM2/components/Home.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
 import VerParaderos from './VerParaderos';
 import PointToPoint from './PointToPoint';
 import SearchRoutes from './SearchRoutes';
@@ -8,25 +9,78 @@ import SearchRoutes from './SearchRoutes';
 const Tab = createBottomTabNavigator();
 
 const Home = () => {
-
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Ver Paraderos" component={VerParaderos} />
-            <Tab.Screen name="Punto a Punto" component={PointToPoint} />
-            <Tab.Screen name="Buscar Rutas" component={SearchRoutes} />
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    height: 100, 
+                },
+                tabBarIconStyle: {
+                    width: 60, // Ajusta el tamaño del área de los íconos
+                    height: 60,
+                },
+            }}
+        >
+            <Tab.Screen 
+                name="Ver Paraderos" 
+                component={VerParaderos}
+                options={{
+                    tabBarLabel: 'Ver Paraderos',
+                    tabBarLabelStyle: {fontWeight: 'bold', fontSize: 15, marginBottom:10},
+                    tabBarActiveTintColor: '#007BFF',
+                    tabBarInactiveTintColor: '#007BFF',
+                    tabBarActiveBackgroundColor: '#D9D9D9',
+                    tabBarInactiveBackgroundColor: 'white',
+                    tabBarIcon:() => (
+                        <AntDesign name='car' size={40} style={{color: '#007BFF'}} />
+                    ),
+                    headerShown: false, // Ocultar el título en la parte superior
+                }}
+            />
+            <Tab.Screen 
+                name="Punto a Punto" 
+                component={PointToPoint}
+                options={{
+                    tabBarLabel: 'Punto a Punto',
+                    tabBarLabelStyle: {fontWeight: 'bold', fontSize: 15, marginBottom:10},
+                    tabBarActiveTintColor: '#007BFF',
+                    tabBarInactiveTintColor: '#007BFF',
+                    tabBarActiveBackgroundColor: '#D9D9D9',
+                    tabBarInactiveBackgroundColor: 'white',
+                    tabBarIcon:() => (
+                        <AntDesign name='flag' size={40} style={{color: '#007BFF'}} />
+                    ),
+                    headerShown: false,
+                }} 
+            />
+            <Tab.Screen 
+                name="Buscar Rutas" 
+                component={SearchRoutes}
+                options={{
+                    tabBarLabel: 'Buscar Rutas',
+                    tabBarLabelStyle: {fontWeight: 'bold', fontSize: 15, marginBottom:10},
+                    tabBarActiveTintColor: '#007BFF',
+                    tabBarInactiveTintColor: '#007BFF',
+                    tabBarActiveBackgroundColor: '#D9D9D9',
+                    tabBarInactiveBackgroundColor: 'white',
+                    tabBarIcon:() => (
+                        <AntDesign name='enviroment' size={40} style={{color: '#007BFF'}} />
+                    ),
+                    headerShown: false,
+                }} 
+            />
         </Tab.Navigator>
     )
 }
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-
     },
     map: {
         width: '100%',
         height: '100%',
     },
-})
+});
--- Proyecto - RutasSM/RutasSM2/components/PointToPoint.js
+++ Proyecto - RutasSM/RutasSM2/components/PointToPoint.js
@@ -1,14 +1,66 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import MapView, { Callout, Circle, Marker, Polyline } from 'react-native-maps'
 
 const PointToPoint = () => {
+    const [origin, setOrigin] = useState(
+        {
+            latitude: -12.056469727385394, 
+            longitude: -77.08442460632264,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.015,
+        }
+    );
+
+    const [destination, setDestination] = useState(
+        {
+            latitude: -12.05571313559246,
+            longitude: -77.04620576494976,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+        }
+    );
+
     return (
-        <View>
-            <Text>PointToPoint</Text>
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={origin}
+            >
+                <Marker
+                    coordinate={origin}
+                    pinColor='skyblue'
+                >
+                    <Callout>
+                        <Text>Esto es un Callout</Text>
+                    </Callout>
+                </Marker>  
+                <Marker
+                    coordinate={destination}
+                    pinColor='purple'
+                >
+                </Marker>
+
+                {/* <Circle center={origin} radius={2000}/>
+                <Polyline 
+                    coordinates={[origin, destination]}
+                    strokeColor='red'
+                    strokeWidth={5}
+                /> */}
+            </MapView>
         </View>
     )
 }
 
 export default PointToPoint
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        
+    },
+    map:{
+        width:'100%',
+        height:'100%',
+    },
+})
