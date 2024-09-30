9ea98fab8a5a9247b1184552fab2cbcfab7dd0c3
JefferVentu <jvruiz62@gmail.com>
2024-09-30 12:52:45 -0500

    Create firabase-config.js

---
+++ Proyecto - RutasSM/RutasSM2/firabase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCW742yg_z4AH80Wyqpb0mLrBVq3NsMDW8",
+    authDomain: "react-native-mayo-24-54b0d.firebaseapp.com",
+    projectId: "react-native-mayo-24-54b0d",
+    storageBucket: "react-native-mayo-24-54b0d.appspot.com",
+    messagingSenderId: "300126817370",
+    appId: "1:300126817370:web:ec39eec430395ea0809c63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = initializeFirestore(app, {experimentalForceLongPolling: true});
+export{ db };
