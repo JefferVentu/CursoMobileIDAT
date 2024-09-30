eb73b7fcf4cd40f071014b620d23fc1c78b32f71
JefferVentu <jvruiz62@gmail.com>
2024-09-30 11:39:04 -0500

    pruebamodal

--- Proyecto - RutasSM/RutasSM2/components/drawer/Settings.js
+++ Proyecto - RutasSM/RutasSM2/components/drawer/Settings.js
@@ -1,40 +1,10 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useNavigation } from '@react-navigation/native';
 
 const Settings = () => {
-
-  const navigation = useNavigation();
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity 
-        style={styles.option} 
-        onPress={() => navigation.navigate('Profile')}
-      >
-        <Text style={styles.optionText}>Opción 1: Perfil</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity 
-        style={styles.option} 
-        onPress={() => navigation.navigate('Notifications')}
-      >
-        <Text style={styles.optionText}>Opción 2: Notificaciones</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity 
-        style={styles.option} 
-        onPress={() => navigation.navigate('Security')}
-      >
-        <Text style={styles.optionText}>Opción 3: Seguridad</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity 
-        style={styles.option} 
-        onPress={() => navigation.navigate('Help')}
-      >
-        <Text style={styles.optionText}>Opción 4: Ayuda</Text>
-      </TouchableOpacity>
+      <Text>Settings</Text>
     </View>
   )
 }
---
+++ Proyecto - RutasSM/RutasSM2/components/settings/Idioma.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Idioma = () => {
+    return (
+        <View>
+            <Text>Idioma</Text>
+        </View>
+    )
+}
+
+export default Idioma
+
+const styles = StyleSheet.create({})
