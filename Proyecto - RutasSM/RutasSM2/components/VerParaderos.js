48460c3fb9b278308dd008bd73d20f32c68b3dcd
JefferVentu <jvruiz62@gmail.com>
2024-10-04 09:13:50 -0500

    Update VerParaderos.js

--- Proyecto - RutasSM/RutasSM2/components/VerParaderos.js
+++ Proyecto - RutasSM/RutasSM2/components/VerParaderos.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import { AntDesign } from '@expo/vector-icons';
 
@@ -11,13 +11,29 @@ const VerParaderos = () => {
         latitudeDelta: 0.010,
         longitudeDelta: 0.010,
     });
+    const [marker, setMarker] = useState(null);
+    const mapRef = useRef(null);
+
+    const moveToLocation = (latitude, longitude) => {
+        const newRegion = {
+            latitude,
+            longitude,
+            latitudeDelta: 0.002, 
+            longitudeDelta: 0.002,
+        };
+
+        setMarker({ latitude, longitude }); 
+        mapRef.current.animateToRegion(newRegion, 1000);
+    };
 
     return (
         <View style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 initialRegion={origin}
             >
+                {marker && <Marker coordinate={marker} />}
             </MapView>
 
             <View style={styles.tituloParadero}>
@@ -25,45 +41,78 @@ const VerParaderos = () => {
             </View>
             
             <ScrollView style={styles.buttonsContainer}>
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.053676542486649, -77.08571968247284)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Sistemas</Text>
                 </TouchableOpacity>
                 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.055477211132514, -77.08209137341198)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Clínica</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.057276063511942, -77.08020980377633)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Puerta 3</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.05952495766934, -77.07962454373136)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Puerta 2</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.060273827569626, -77.08061159662596)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Industrial</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.060744657124024, -77.08294645953667)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Comedor</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.05999710004744, -77.08446324880997)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Gimnasio</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.055928079704337, -77.08493500096182)}
+                >
                     <AntDesign name="enviromento" size={20} style={styles.icon} />
                     <Text>P. Biblioteca</Text>
                 </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={() => moveToLocation(-12.054863118999455, -77.08620100359333)}
+                >
+                    <AntDesign name="enviromento" size={20} style={styles.icon} />
+                    <Text>P. Odontología</Text>
+                </TouchableOpacity>
+
                 <View style={styles.space}>
                 </View>
             </ScrollView>
